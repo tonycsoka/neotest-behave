3fe9720cc52df6c86170520110899f3335a8327d
Tony Csoka <Tony.Csoka@gmail.com>
2024-12-19 06:09:28 +0000

    try and parse the line and catch the error

--- lua/neotest-behave/core/result_builder.lua
+++ lua/neotest-behave/core/result_builder.lua
@@ -14,24 +14,25 @@ function ResultBuilder.build_results(spec, result, tree)
 
 	local success, lines = pcall(lib.files.read_lines, result.output)
 	for _, line in ipairs(lines) do
-		if line == "[" or line == "]" or line == "" then
-			goto continue
-		end
 		line = string.gsub(line, "^(.-),?$", "%1")
 		local ok, bdd_json = pcall(vim.json.decode, line, { luanil = { object = true } })
+		if not ok then
+			goto continue
+		end
 
 		local file = vim.split(bdd_json.location, ":")[1]
 		local feature = bdd_json.name
 		for _, element in ipairs(bdd_json.elements) do
 			if element.type == "scenario" then
 				local name = element.name
-				run_res[file .. "::" .. feature .. "::" .. name] = {
+				local element_name = file .. "::" .. feature .. "::" .. name
+				run_res[element_name] = {
 					status = element.status,
 				}
 				if element.status == "failed" then
 					for _, step in ipairs(element.steps) do
 						if step.result ~= nil and step.result.status == "failed" then
-							run_res[file .. "::" .. feature .. "::" .. name].short = step.location
+							run_res[element_name].short = step.location
 						end
 					end
 				end
